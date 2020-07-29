e0c0089d45e40302fc423c655eaef50445ec62e0
deena_d <silja.rheingans@gmail.com>
2020-07-29 15:18:33 +0200

    So, I'm trying to push my newly initialized Git directory to the remote repository. Hope it works this time!

---
+++ premake5.lua
@@ -0,0 +1,97 @@
+workspace "hazzzel" 
+    architecture "x64"
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "hazzzel"
+    location "hazzzel"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs {
+	    "%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+	    cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+            "HZ_PLATFORM_WINDOWS",
+			"HZ_BUILD_DLL", 
+			"_WINDLL"
+		}
+
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+	    defines "HZ_DEBUG" 
+		symbols "On"
+
+	filter "configurations:Release"
+	    defines "HZ_RELEASE" 
+		optimize "On"
+
+	filter "configurations:Dist"
+	    defines "HZ_DIST" 
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox" 
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs	{
+	    "hazzzel/vendor/spdlog/include",
+        "hazzzel/src"	
+	}
+
+	links {
+	    "hazzzel"
+	}
+
+	filter "system:windows"
+	    cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+            "HZ_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+	    defines "HZ_DEBUG" 
+		symbols "On"
+
+	filter "configurations:Release"
+	    defines "HZ_RELEASE" 
+		optimize "On"
+
+	filter "configurations:Dist"
+	    defines "HZ_DIST" 
+		optimize "On"
